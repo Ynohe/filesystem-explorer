d360889d5ae1913b24423161a5d62037f7364773
Ynohe <ynotimelesss@gmail.com>
2023-01-12 17:04:38 +0000

    show the icons

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 	$formats   = array('.jpg', '.png', '.gif', '.doc', '.csv', '.txt', '.pdf', '.zip', '.rar', '.mp4', '.mp3');
 	$directory = 'root/assets';
-	$extension = glob("root/assets/*.png");
-    $time = $_FILES['archivo'];
+    $url_insert = dirname(__DIR__) . "/root";
 	if (isset($_POST['boton'])){
 		$name  = $_FILES['archivo']['name'];
 		$saved = $_FILES['archivo']['tmp_name'];
@@ -34,10 +33,13 @@
                         while ($archive = readdir($dir)) {
                             if ($archive != '.' && $archive != '..') {
                                 echo '<div class="col-sm-3 col-xs-12">';
-                                echo "Archive: <strong>$archive</strong><br />";
-                                echo $_FILES['archivo']['size'] / 1000000;
+                                echo "Archive: <strong>$archive  </strong><br />";
+                                echo '<img src="'.$directory.'/'.$archive.'" width = 300px title="imagen" alt="imagen"/>';
+                                echo $_FILES['archivo']['size'] / 1000000 ,"bytes";
                                 echo "The last modification date was: " . date ('F d Y H:i:s.', filectime($directory.'/'.$archive));
-								var_dump($extension);
+                                // if(pathinfo($dir)["extension"])
+                                $url_target = str_replace('\\', '/', $url_insert) . '/' . $archive;
+                                echo '<img src="'.$directory.'/logo'. '/' .pathinfo($archive)["extension"]. '.png"'. '/>';
                                 }
                             }   
                         }
